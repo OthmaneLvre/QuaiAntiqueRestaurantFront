db127970ff130defc483260aa8fbf1cf93301b55
Othmane Lvre <othmane.lecoeuvre@gmail.com>
2024-11-04 18:10:59 +0100

    Carte finish

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,11 +4,13 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/carte", "La carte", "/pages/carte.html"),
     new Route("/signin", "Connexion", "/pages/auth/signin.html"),
     new Route("/signup", "Inscription", "/pages/auth/signup.html"),
     new Route("/account", "Mon compte", "/pages/auth/account.html"),
     new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
     new Route("/allResa", "Vos réservations", "/pages/reservations/allResa.html"),
+    new Route("/reserver", "Ma reservation", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
