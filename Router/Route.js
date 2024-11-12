a5e333b73b13a53f60f1405cdc85df64c037637a
Othmane Lvre <othmane.lecoeuvre@gmail.com>
2024-11-12 19:34:10 +0100

    Dynamiser la partie front

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,18 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
+/*
+[] -> Tout le monde peut y accéder 
+ ["disconnected"] -> Réserver aux utilisateurs déconnecté
+  ["disconnected"] -> Réserver aux utilisateurs avec le role client
+   ["admin"]-> Réserver aux utilisateurs avec le role admin 
+   ["admin", "client"] -> Réserver aux utilisateurs avec le role client ou admin 
+
+*/
