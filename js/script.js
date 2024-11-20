f5c4664534b3e441d89d90861b4772b1af174572
Othmane Lvre <othmane.lecoeuvre@gmail.com>
2024-11-20 15:19:05 +0100

    Eviter faille XSS injection HTML

--- js/script.js
+++ js/script.js
@@ -92,3 +92,9 @@ function showAndHideElementsForRoles(){
         }
     })
 }
+
+function sanitizeHtml(text){
+    const tempHtml = document.createElement('div');
+    tempHtml.textContent = text;
+    return tempHtml.innerHTML;
+}
